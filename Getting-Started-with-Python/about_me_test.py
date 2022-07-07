97f2906110a317e66163e7fa36e078c58d200f41
Chairmantee <chairmanteedragon@outlook.com>
2022-07-07 01:51:39 -0400

    adding 'getting starting with python' activity

---
+++ Getting-Started-with-Python/about_me_test.py
@@ -0,0 +1,24 @@
+full_name = "Oluwatobiloba Aiyewunmi"
+preferred_nickname = "Chairman Tee"
+age = 23
+python_experience = True
+
+favorite_hobbies = [
+  "watching tv shows", "playing video/mobile games", 
+  "going to the beach" 
+]
+
+favorite_things = {
+  "movie": "Harry Potter",
+  "food": "Jollof rice",
+  "hobby": "playing mobile games",
+  "mobile_game": "dream league soccer"
+}
+
+print(full_name, preferred_nickname, age, python_experience)
+print()
+print(favorite_hobbies)
+print()
+print(favorite_things)
+
+# print(full_name, preferred_nickname, age, python_experience, "\n", "]\n", favorite_hobbies, "\n", "\n", favorite_things)
